0894a73265fb0a8c137f0129ff709e35ce60ca19
AmanNegi27 <115018060+AmanNegi27@users.noreply.github.com>
2025-09-19 10:13:28 +0530

    Add files via upload

---
+++ MyForm.cpp
@@ -0,0 +1,15 @@
+﻿#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+int main(array<System::String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	CompilerUI::MyForm form;
+	Application::Run(% form);
+
+	return 0;
+}
